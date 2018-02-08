79aa02b51a6e32902a8c3b5091e4aa05a355fe86
DESKTOP-BQNBV80\Lars Moesman <morty@gmail.com>
2018-02-08 16:48:11 +0100

    Made Exercise B2

--- Microcontrollers_Week2/Microcontrollers_Week2/src/main.c
+++ Microcontrollers_Week2/Microcontrollers_Week2/src/main.c
@@ -1,40 +1,88 @@
-/**
- * \file
- *
- * \brief Empty user application template
- *
- */
-
-/**
- * \mainpage User Application template doxygen documentation
- *
- * \par Empty user application template
- *
- * Bare minimum empty user application template
- *
- * \par Content
- *
- * -# Include the ASF header files (through asf.h)
- * -# "Insert system clock initialization code here" comment
- * -# Minimal main function that starts with a call to board_init()
- * -# "Insert application code here" comment
- *
- */
-
 /*
- * Include header files for all drivers that have been imported from
- * Atmel Software Framework (ASF).
- */
-/*
- * Support and FAQ: visit <a href="http://www.atmel.com/design-support/">Atmel Support</a>
- */
-#include <asf.h>
+ * Microcontrollers_Week2.c
+ *
+ * Created: 8-2-2018 16:21:46
+ * Author : Lars Moesman & Rick Verstraten
+ */ 
 
-int main (void)
-{
-	/* Insert system clock initialization code here (sysclk_init()). */
+#include "main.h"
+#include <avr/io.h>
+#include <avr/interrupt.h>
 
-	board_init();
-
-	/* Insert application code here, after the board has been initialized. */
+/******************************************************************/
+void ExecuteB2(){
+/* 
+short:			Initilises interreupts and io
+inputs:			
+outputs:	
+notes:			
+Version :    	1.0
+Author	:		Lars Moesman & Rick Verstraten
+*******************************************************************/
+	DDRC = 0xFF;
+	DDRD = 0x00;
+	
+	PORTC = 0x01;
+	
+	EICRA |= 0x3C;
+	EIMSK |= 0x06;
+	
+	sei();
 }
+
+/******************************************************************/
+ISR(INT1_vect) {
+/*
+short:			ISR INT1
+inputs:
+outputs:
+notes:			Set PORTC
+Version :    	1.0
+Author	:		Lars Moesman & Rick Verstraten
+*******************************************************************/
+	if (PORTC == 0x80)
+	{
+		PORTC = 0x01;
+	}else {
+			PORTC <<= 1;
+	}
+
+}
+
+/******************************************************************/
+ISR(INT2_vect) {
+/*
+short:			ISR INT2
+inputs:
+outputs:
+notes:			Set PORTC
+Version :    	1.0
+Author	:		Lars Moesman & Rick Verstraten
+*******************************************************************/
+	if (PORTC == 0x00)
+	{
+		PORTC = 0x80;
+	}else {
+		PORTC >>= 1;
+	}
+	
+}
+
+/******************************************************************/
+int main(void)
+/*
+short:			main() loop, entry point of executable
+inputs:
+outputs:
+notes:			Looping forever, flipping bits on PORTD
+Version :    	DMK, Initial code
+*******************************************************************/
+{
+	ExecuteB2();
+	
+    while (1) 
+    {
+	
+    }
+}
+
---
+++ Microcontrollers_Week2/Microcontrollers_Week2/src/main.h
@@ -0,0 +1,16 @@
+/*
+ * main.h
+ *
+ * Created: 8-2-2018 16:19:23
+ *  Author: Lars Moesman
+ */ 
+
+
+#ifndef MAIN_H_
+#define MAIN_H_
+#define F_CPU 8000000
+
+void ExecuteB2(void);
+
+
+#endif /* MAIN_H_ */
